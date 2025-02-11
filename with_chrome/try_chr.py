bddcb0f83397b3ef19c3f93c6d8161362c2809c6
Ankur586 <ankur.singh586@gmail.com>
2025-02-11 23:23:53 +0530

    status code with_chrome

--- with_chrome/try_chr.py
+++ with_chrome/try_chr.py
@@ -1,35 +1,36 @@
-from selenium.webdriver.chrome.service import Service
-from selenium import webdriver
-
-from with_chrome.setup_chrome import WebDriverSetup
-
 import json
+from requests.structures import CaseInsensitiveDict
+
+from selenium import webdriver
+from selenium.webdriver.chrome.service import Service
+
+from webdriver_manager.chrome import ChromeDriverManager
 
 from fake_useragent import UserAgent
 
 ua = UserAgent()
-
+users_agent = ua.random
 url = 'https://nourishstore.in/'
 
 chrome_options = webdriver.ChromeOptions()
-chrome_options.add_argument(f"user-agent={ua}")
+chrome_options.add_argument(f"user-agent={users_agent}")
 chrome_options.add_argument("--headless=new")
-
-
-web_driver_setup = WebDriverSetup(headless=True)
-driver = web_driver_setup.setup_driver()
-
-
-status_code_driver = driver.get(url)
-
-# Parse the Chrome Performance logs
+chrome_options.set_capability("goog:loggingPrefs", {"performance": "ALL"})
+driver = webdriver.Chrome(
+    service=Service(ChromeDriverManager().install()), options=chrome_options
+        )
+driver.get(url)
 responses = []
 response = None
-for log_entry in status_code_driver.get_log("performance"):
+for log_entry in driver.get_log("performance"):
     log_message = json.loads(log_entry["message"])["message"]
     # Filter out HTTP responses
     if log_message["method"] == "Network.responseReceived":
         responses.append(log_message["params"]["response"])
         if log_message["params"]["type"] == "Document":
             response = log_message["params"]["response"]
-print(response)
+# print(response)
+# headers = CaseInsensitiveDict(response["headers"])
+status_code = response["status"]
+print(f"HTTP Status code: {status_code}")
+# print(f"Headers: {headers}")
