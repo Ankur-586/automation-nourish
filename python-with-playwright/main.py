a53e9863f0c734adb8563cf88db03788fe621a19
Ankur586 <ankur.singh586@gmail.com>
2025-02-17 16:43:42 +0530

    code added in playwrigt folder

--- python-with-playwright/main.py
+++ python-with-playwright/main.py
@@ -2,12 +2,32 @@ from playwright.sync_api import sync_playwright
 
 def website_search(site_url):
     with sync_playwright() as p:
-        browser = p.chromium.launch(headless=False)
+        browser = p.chromium.launch(headless=True)
         page = browser.new_page()
         page.goto(site_url)
         print(page.title())
         page.click('#autocompleteInput')
-        page.fill('#autocompleteInput', 'Dal')
+        page.fill('#autocompleteInput', 'Rice')
+        page.wait_for_timeout(1000)
+        
+        # locator = page.locator("//ul//div[contains(@class, 'flex justify-between items-center')]").all()
+        # # locator.wait_for(state="visible")  # wait for the element to be visible
+        # # x = page.getByText('//ul//div[contains(@class, "flex justify-between items-center")]')
+        # for product in locator:
+        #     product_name = product.(By.XPATH, self.fetched_product_name).text
+        
+        locator = page.locator("//ul//div[contains(@class, 'flex justify-between items-center')]")
+        count = locator.count()
+        product_list = set()
+        for product in range(count):
+            element = locator.nth(product)
+            product_list.add(element.text_content())
+        lst = list(product_list)
+        # print(lst)
+        for i in lst:
+            x = i.find(")")
+            print(i[0:x+1].strip())
+        
         browser.close()
     
 url = 'https://nourishstore.co.in/'
